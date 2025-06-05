177abb5109634fb799b1957748602b628d457413
Fkll <fook165@gmail.com>
2025-06-05 16:12:44 +0700

    True/Flase1

--- Myproject/settings.py
+++ Myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xug1i+ovuju*$6lxxtqubf6r07k_ff!i6b9zsk%qyx+p_fs5+a
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['smart-meeting-room.onrender.com']
 # smart-meeting-room.onrender.com
 # localhost', '127.0.0.1', '172.30.10.169', 'https://04dd-49-228-178-104.ngrok-free.app
 # Application definition
